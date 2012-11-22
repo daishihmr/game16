df8a4b7911156a4134fe4a920fa14c12ec97e71c
daishihmr <daishi.hmr@gmail.com>
2012-11-22 13:31:09 +0900

    stars

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require(tm.app);
 require(tm.graphics);
+require(tm.geom);
 
 tm.preload(function() {
     TextureManager.add("kuma", "chara1.png");
@@ -8,34 +9,44 @@ tm.preload(function() {
 tm.main(function() {
     var app = CanvasApp("#main");
     app.fitWindow();
-    app.background = "rgba(0,0,0,0.5)";
+    app.background = "rgba(0,0,0,0.3)";
 
     var kuma = Sprite(32, 32, TextureManager.get("kuma"));
     kuma.x = (app.width - kuma.width) / 2;
     kuma.y = (app.height - kuma.height) / 2;
     kuma.frame = 0;
+    kuma.stars = [];
     kuma.update = function() {
         this.setFrameIndex(this.frame);
     };
     app.currentScene.addChild(kuma);
 
-    var a = Math.PI * 2 / 3;
-    for ( var i = 0; i < 3; i++) {
-        var star = Shape(32, 32);
-        star.index = i;
-        star.canvas.setColorStyle("white", "yellow").fillStar(16, 16, 16, 5);
-        star.update = function() {
-            this.x = Math.cos(app.frame * 0.2 + a * this.index) * 100;
-            this.y = Math.sin(app.frame * 0.2 + a * this.index) * 100;
-        };
-        kuma.addChild(star);
-    }
+    var Star = tm.createClass({
+        superClass : Shape,
+        init : function() {
+            this.superInit(32, 32);
+            this.canvas.setColorStyle("white", "yellow")
+                    .fillStar(16, 16, 16, 5);
+        },
+        update : function() {
+            var a = Math.PI * 2 / kuma.stars.length;
+            var index = kuma.stars.indexOf(this);
+            this.x = Math.cos(app.frame * 0.2 + a * index) * 100;
+            this.y = Math.sin(app.frame * 0.2 + a * index) * 100;
+        }
+    });
 
     app.currentScene.update = function(a) {
         if (a.pointing.getPointingStart()) {
             var x = a.pointing.x;
             var y = a.pointing.y;
             kuma.animation.moveTo(x, y, 1000, "easeInOutQuad");
+        }
+
+        if (app.frame % 150 === 0) {
+            var newStar = new Star();
+            kuma.stars.push(newStar);
+            kuma.addChild(newStar);
         }
     };
 
