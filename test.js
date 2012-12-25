e352d320b1cc700f57819855455c5a2221d16951
daishihmr <daishi.hmr@gmail.com>
2012-12-25 11:16:21 +0900

    add test.html

---
+++ test.js
@@ -0,0 +1,54 @@
+var SCREEN_WIDTH = 320;
+var SCREEN_HEIGHT = 420;
+var BORDER_WIDTH = 4;
+
+tm.main(function() {
+
+    var app = tm.app.CanvasApp("#world");
+
+    var _fitFunc = function() {
+        if (window.innerWidth > window.innerHeight) {
+            app.resize(SCREEN_HEIGHT + BORDER_WIDTH*2, SCREEN_HEIGHT + BORDER_WIDTH*2);
+        } else {
+            app.resize(SCREEN_WIDTH + BORDER_WIDTH*2,
+                (SCREEN_WIDTH + BORDER_WIDTH*2) * window.innerHeight/window.innerWidth);
+        }
+
+        var e = this.element;
+        var s = e.style;
+
+        s.position = "absolute";
+        s.left = "0px";
+        s.top  = "0px";
+
+        var rateWidth = e.width/window.innerWidth;
+        var rateHeight= e.height/window.innerHeight;
+        var rate = e.height/e.width;
+
+        if (rateWidth > rateHeight) {
+            s.width  = innerWidth+"px";
+            s.height = innerWidth*rate+"px";
+        }
+        else {
+            s.width  = innerHeight/rate+"px";
+            s.height = innerHeight+"px";
+        }
+    }.bind(app);
+
+    // 一度実行しておく
+    _fitFunc();
+    // リサイズ時のリスナとして登録しておく
+    window.addEventListener("resize", _fitFunc, false);
+    window.addEventListener("orientationchange", _fitFunc, false);
+
+    var screen = CanvasScreen(SCREEN_WIDTH, SCREEN_HEIGHT);
+    screen.x = screen.width / 2 + BORDER_WIDTH;
+    screen.y = screen.height / 2 + BORDER_WIDTH;
+    screen.drawStroke = true;
+    app.currentScene.addChild(screen);
+
+    app.currentScene.update = function() {
+    };
+
+    app.run();
+});
---
+++ util.js
@@ -0,0 +1,52 @@
+var CanvasScreen = tm.createClass({
+    superClass: tm.app.CanvasElement,
+    init: function(width, height) {
+        this.superInit();
+        this.width = width;
+        this.height = height;
+        this.background = "black";
+        this.drawStroke = false;
+        this.myCanvas = tm.graphics.Canvas();
+        this.myCanvas.resize(width, height);
+    },
+    _draw: function(canvas) {
+        this.myCanvas.clearColor(this.background, 0, 0);
+
+        if (this.visible === false) return;
+
+        if (this.children.length > 0) {
+            var tempChildren = this.children.slice();
+            for (var i = 0, len = tempChildren.length; i < len; i++) {
+                tempChildren[i]._draw(this.myCanvas);
+            }
+        }
+
+        var context = canvas.context;
+
+        context.save();
+
+        context.fillStyle = this.fillStyle;
+        context.strokeStyle = this.strokeStyle;
+        context.globalAlpha *= this.alpha;
+        context.globalComppositeOperation = this.blendMode;
+
+        if (this.shadowBlur > 0) {
+            context.shadowColor = this.shadowColor;
+            context.shadowOffsetX = this.shadowOffsetX;
+            context.shadowOffsetY = this.shadowOffsetY;
+            context.shadowBlur = this.shadowBlur;
+        }
+
+        context.translate(this.position.x, this.position.y);
+        context.rotate(this.rotation * Math.DEG_TO_RAD);
+        context.scale(this.scale.x, this.scale.y);
+
+        context.drawImage(this.myCanvas.canvas,
+            0, 0, this.width, this.height,
+            -this.width*this.originX, -this.height*this.originY, this.width, this.height);
+
+        if (this.drawStroke) this.drawStrokeRect(context);
+
+        context.restore();
+    }
+});
